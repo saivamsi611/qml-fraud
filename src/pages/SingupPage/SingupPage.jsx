6e2d72380f73977dd7e887c95d285daa1ff87278
nsky26 <nanneboinasai2005@gmail.com>
2025-08-18 13:19:28 +0530

    Add my changes after merging main

---
+++ src/pages/SingupPage/SingupPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './SingupPage.css';
+
+export default function SignupPage() {
+  return (
+    <div className="wrapper">
+      <div className="console-panel">
+        <h2>Sign up here</h2>
+        <form>
+          <label>Username</label>
+          <input type="text" placeholder="username " required />
+
+          <label>Email</label>
+          <input type="email" placeholder="email " required />
+
+          <label>Password</label>
+          <input type="password" placeholder="Authorization key" required />
+
+          <div className="button-group">
+            <button type="submit">Launch</button>
+            <button type="reset">Reset</button>
+          </div>
+        </form>
+
+        <p className="form-link">
+          Already registered? <Link to="/login">Login to deck</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
